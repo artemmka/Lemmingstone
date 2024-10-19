b5109b6cf104ff3caf0caf30ef6fabf98e569ee0
NemesisGoat <82102672+NemesisGoat@users.noreply.github.com>
2024-10-19 19:04:34 +0400

    Генерация карты

---
+++ lemmingstone/client/src/services/server/types.ts
@@ -0,0 +1,45 @@
+export type TError = {
+    code: number;
+    text: string;
+}
+
+export type TAnswer<T> = {
+    result: 'ok' | 'error';
+    data?: T;
+    error?: TError;
+}
+
+export type TUser = {
+    token: string;
+    name: string;
+}
+
+export type TMessage = {
+    message: string;
+    author: string;
+    created: string;
+}
+
+export type TMessages = TMessage[];
+export type TMessagesResponse = {
+    messages: TMessages;
+    hash: string;
+}
+
+export type TCoeffs = {
+    a: number[],
+    b: number[],
+    c: number[],
+    d: number[],
+    h: number[]
+}
+
+export type TPoint = {
+    x: number;
+    y: number;
+}
+
+export type TPointsAndSplines = {
+    coeffs: TCoeffs,
+    points: TPoint[];
+}
