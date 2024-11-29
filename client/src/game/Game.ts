d03c9c3c8f8e9765d31e67dca97ef439f42c9dab
NemesisGoat <82102672+NemesisGoat@users.noreply.github.com>
2024-11-29 21:13:03 +0400

    Офигеть оно бахает

--- client/src/game/Game.ts
+++ client/src/game/Game.ts
@@ -19,7 +19,7 @@ class Game {
     private WINDOW: { LEFT: number, TOP: number, HEIGHT: number, WIDTH: number };
 
     constructor(canvas: Canvas, WINDOW: { LEFT: number, TOP: number, HEIGHT: number, WIDTH: number }) {
-        this.kapitoshka = { x: 2, y: 5 };
+        this.kapitoshka = { x: 2, y: -5 };
         this.canvas = canvas;
         this.moving = setInterval(() => this.velocity(), 5);
         this.WINDOW = WINDOW;
@@ -68,6 +68,8 @@ class Game {
         this.explosions.push({ x: this.kapitoshka.x, y: this.kapitoshka.y });
     }
 
+
+    //Надо будет как-то разнести на несколько функций
     move(dx: number, dy: number): void {
         if ((this.checkCollision(this.kapitoshka.x, this.kapitoshka.y, 'right') || this.checkCollision(this.kapitoshka.x, this.kapitoshka.y, 'left')) && this.dx != 0) {
             this.dy = 0;
@@ -85,10 +87,11 @@ class Game {
             }
             this.kapitoshka.x += dx;
         }
-        if ((dy > 0 && this.kapitoshka.y + dy <= HEIGHT - 1 && !this.checkCollision(this.kapitoshka.x, this.kapitoshka.y, 'down')) ||
-            (dy < 0 && this.kapitoshka.y - dy >= 0 && !this.checkCollision(this.kapitoshka.x, this.kapitoshka.y, 'up'))
+        if ((dy > 0 && !this.checkCollision(this.kapitoshka.x, this.kapitoshka.y, 'down')) ||
+            (dy < 0 && !this.checkCollision(this.kapitoshka.x, this.kapitoshka.y, 'up'))
         ) {
             this.kapitoshka.y += dy;
+            this.WINDOW.TOP = this.kapitoshka.y - this.WINDOW.HEIGHT/2 - 0.5;
         }
     }
 
